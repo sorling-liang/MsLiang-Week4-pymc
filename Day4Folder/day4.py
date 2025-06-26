d9d95dc7da8d6c1cf9493637ed03651daf7515a5
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-26 02:11:12 +0000

    Jun 26, 2025, 2:11 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -41,7 +41,9 @@ print("hello from day4")
 import random
 num1 = random.randint(5, 69)
 num2 = random.randint(5, 69)
+
 hidden = num1 + num2
+
 question = "what is " + str(num1) + " + " + str(num2) + "? "
 guess = input(question)
 tries = 1
