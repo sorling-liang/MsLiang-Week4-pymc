14d9f6ca909273ea6628491243fbb5bff7fdfe47
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-26 01:40:41 +0000

    Jun 26, 2025, 1:40 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -4,7 +4,7 @@ print("hello from day4")
 
 import random
 for count in range(10):
-    diceValue = random.randint(100, 300)
+    diceValue = random.randint(1000, 9999)
     print(diceValue)
 
 ########################################################################
