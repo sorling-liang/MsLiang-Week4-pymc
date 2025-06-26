fafa6afed64b5664b07a9641e49cb1fbe1e18d9f
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-26 02:07:45 +0000

    Jun 26, 2025, 2:07 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -2,10 +2,10 @@
 # COMMENT out the previous task before going on to the next task
 print("hello from day4")
 
-import random
-for count in range(10):
-    diceValue = random.randint(1, 9999)
-    print(diceValue)
+# import random
+# for count in range(10):
+#     diceValue = random.randint(1, 9999)
+#     print(diceValue)
 
 ########################################################################
 # Task 1:
@@ -15,16 +15,16 @@ for count in range(10):
 ########################################################################
 # Task 2:
 
-riddle = "what do you call a deer with no eyes?"
-hidden = "no idea"
-guess = input( riddle )
-tries = 1
-while guess != hidden:
-    print("wrong! try again!")
-    tries = tries + 1
-    guess = input( riddle ) # ask again
-else:
-    print("you are correct! and you got it after " + str(tries) + " tries.")
+# riddle = "what do you call a deer with no eyes?"
+# hidden = "no idea"
+# guess = input( riddle )
+# tries = 1
+# while guess != hidden:
+#     print("wrong! try again!")
+#     tries = tries + 1
+#     guess = input( riddle ) # ask again
+# else:
+#     print("you are correct! and you got it after " + str(tries) + " tries.")
 
 ########################################################################
 # Additional exercises:
